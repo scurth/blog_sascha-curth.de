4e6039a89bbe5835b4c439f8829fb139e77f4909
Sascha Curth <github@sascha-curth.de>
2020-02-29 20:12:47 +0100

    favicon and some links

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,11 +5,12 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'IoT Kompendium', link: '/kompendium/' },
-      { text: 'IoT Projekte', link: '/projekte/' },
+      { text: 'IoT Kompendium', link: '/kompendium/index.html' },
+      { text: 'IoT Projekte', link: '/projekte/index.html' },
     ],
     sidebar: {
       '/kompendium/': [
+        '/kompendium/',
         '/kompendium/001_Einleitung',
         '/kompendium/002_Miflora',
         '/kompendium/003_Tasmota',
@@ -29,7 +30,30 @@ module.exports = {
      '@vuepress/back-to-top',
   ],
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/favicon/manifest.json' }],
+    ['link', { rel: 'icon', href: '/favicon/favicon.ico' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', href: '/favicon/apple-icon-' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '57x57', href: '/favicon/apple-icon-57x57.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '60x60', href: '/favicon/apple-icon-60x60.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '72x72', href: '/favicon/apple-icon-72x72.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '76x76', href: '/favicon/apple-icon-76x76.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '114x114', href: '/favicon/apple-icon-114x114.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '120x120', href: '/favicon/apple-icon-120x120.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '144x144', href: '/favicon/apple-icon-144x144.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '152x152', href: '/favicon/apple-icon-152x152.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-icon-180x180.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '192x192',  href: '/favicon/android-icon-192x192.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon/favicon-96x96.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' }],
     ['meta', {name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0"}],
+    ['meta', {name: "msapplication-TileColor", content: "#ffffff"}],
+    ['meta', {name: "msapplication-TileImage", content: "/favicon/ms-icon-144x144.png"}],
+    ['meta', {name: "theme-color", content: "#ffffff"}],
   ],
 }
+
+
+
+
